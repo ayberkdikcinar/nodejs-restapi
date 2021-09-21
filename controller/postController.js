5c36609223b9d0ca528edc77c45be6f63430a03c
ayberkdikcinar <ayberk.dikcinar@ceng.deu.edu.tr>
2021-09-22 01:01:42 +0300

    post operations are updated

--- controller/postController.js
+++ controller/postController.js
@@ -6,10 +6,13 @@ const createError = require('http-errors');
 const getMyPosts = async (req,res,next)=>{
     ///tarihe göre verilen sayıya göre. 
     try{
-        const result = await Post.find({"owner":req.user.userID}).limit(5).sort({'createdAt':-1}).skip(Number(req.params.number));
-        //const result = await Post.find({"owner":req.user._id}).limit(5).sort({'createdAt':-1}).lte({'createdAt':'10.09.2020'});
+        //const result = await Post.find({"owner":req.user._id}).limit(5).sort({'createdAt':-1}).skip(Number(req.params.number));
+        const result = await Post.find({"owner":req.user._id}).limit(Number(req.params.number))
+        .sort({'createdAt':-1}).where('createdAt').gte(req.params.date);
+
         if(result){
-            return res.status(200).json({result:result,sonGetirilenPost:Number(req.params.number)+4});
+            const lastDate=result[(result.length)-1].createdAt;
+            return res.status(200).json({result:result,sonGetirilenPostTarihi:lastDate});
         }
         throw createError(404,'there is no post found for user id: '+req.user.userID);
     }catch(err){
@@ -32,19 +35,63 @@ const getFollowingPosts = async(req,res,next)=>{
     }
 };
 
-const sendPost = async (req,res)=>{
-    
-    const post = new Post(req.body);
-    post.owner = req.user.userID;
-    const result =await post.save();
-    if(result){
-        console.log('Post has been added to db');
-        return res.status(200).json(result);
+const getLimitedPostByUserId = async (req,res,next)=>{
+    ///tarihe göre verilen sayıya göre. 
+    try{
+
+        const user = await User.findOne({'userID':req.params.userId});
+
+        const result = await Post.find({"owner":user._id}).limit(Number(req.params.number))
+        .sort({'createdAt':-1}).where('createdAt').gte(req.params.date);
+
+        if(result){
+            const lastDate=result[(result.length)-1].createdAt;
+            return res.status(200).json({result:result,sonGetirilenPostTarihi:lastDate});
+        }
+        throw createError(404,'there is no post found for user id: '+user.userID);
+    }catch(err){
+        next(err);
     }
+};
+
+const getAllPostsByUserId = async(req,res,next)=>{
+    try{
+        const user = await User.findOne({'userID':req.params.userId});
+    
+        const result = await Post.find({"owner":user._id}).limit(Number(req.params.number)).sort({'createdAt':-1})
+        .where('createdAt').gte(req.params.date);
+        
+
+        if(result){
+            const lastDate=result[(result.length)-1].createdAt;
+            return res.status(200).json({result:result,sonGetirilenPostTarihi:lastDate});
+        }
+        throw createError(404,'there is no post found for user id: '+user.userID);
+    }catch(err){
+        next(err);
+    }
+
+}
+const sendPost = async (req,res)=>{
+    try{
+        const post = new Post(req.body);
+        post.owner = req.user._id;
+        const result =await post.save();
+        if(result){
+            console.log('Post has been added to db');
+            return res.status(200).json(result);
+        }
+        throw createError(501,'adding post failed');
+    }catch(err){
+        next(err);
+    }
+    
 };
 
 module.exports = {
     sendPost,
     getFollowingPosts,
     getMyPosts,
+    getAllPostsByUserId,
+    getLimitedPostByUserId
 }
--- router/postRouter.js
+++ router/postRouter.js
@@ -5,16 +5,18 @@ const authMiddleware = require('../middleware/authMW');
 
 ///login olmuş kullanıcın verilen sayıdan başlayarak güncel tarih sırasına göre 5 tane kendi postlarını getirir.
 //post listesini ve son listelenen postun numarasını döndürür.
-router.get('/me/:date:number',authMiddleware,postController.getMyPosts);
+router.get('/me/:date/:number',authMiddleware,postController.getMyPosts);
 
 // takip ettiği kişilerin tüm postları arasından verilen sayıdan başlayarak en güncel 5 tanesini getirir.
 //post listesini ve son listelenen postun numarasını döndürür. 
-router.get('/followings/:number',authMiddleware,postController.getFollowingPosts);
+router.get('/followings/:date/:number',authMiddleware,postController.getFollowingPosts);
 
 /// login olmuş kullanıcının post atma işlemi
 router.post('/',authMiddleware,postController.sendPost);
 
+router.get('/all/:userId',authMiddleware,postController.getAllPostsByUserId);
 //herokuapp.movieet.com/api/post/
+router.get('/:userId/:date/:number',authMiddleware,postController.getLimitedPostByUserId);
 //body -> post modeldeki şeyi koyuyon
 
 
